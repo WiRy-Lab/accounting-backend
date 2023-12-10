21e1d4fbba62b8c09a5f6bc98c74feade7b13806
Ryan Lee <lee98064@gmail.com>
2023-12-10 17:42:01 +0800

    Fix user api url error

--- user/urls.py
+++ user/urls.py
@@ -8,7 +8,7 @@ from user import views
 app_name = "auth"
 
 urlpatterns = [
-    path("register/", views.CreateUserView.as_view(), name="register"),
-    path("login/", views.CreateTokenView.as_view(), name="login"),
-    path("me/", views.ManageUserView.as_view(), name="me"),
+    path("register", views.CreateUserView.as_view(), name="register"),
+    path("login", views.CreateTokenView.as_view(), name="login"),
+    path("me", views.ManageUserView.as_view(), name="me"),
 ]
